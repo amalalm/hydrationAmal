e483eaa029406d21999d3e90b8373be381f551c5
amalalm <amalmoh.mtr@gmail.com>
2024-10-20 14:25:40 +0300

    Initial Commit

---
+++ hydrationAmal/hydrationAmalApp.swift
@@ -0,0 +1,17 @@
+//
+//  hydrationAmalApp.swift
+//  hydrationAmal
+//
+//  Created by Amal almutairi on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct hydrationAmalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
